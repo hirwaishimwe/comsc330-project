8aef3f3c60ff7850fa50fd5215adca22de85463a
Hirwa Ishimwe <118475346+hirwaishimwe@users.noreply.github.com>
2023-04-14 10:33:15 -0400

    update src

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from run import run
-
-if __name__ == "__main__":
-
-    #input a file and create a run obj
-    this_run = run (input("Enter Run file: "))
-
-    #get the run output
-    this_run.output()
