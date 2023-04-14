ba1b20f2fdd53de997d5d3a09654ef7344c656fb
Hirwa Ishimwe <118475346+hirwaishimwe@users.noreply.github.com>
2023-04-14 10:42:10 -0400

    update main

--- test/main.py
+++ test/main.py
@@ -1,9 +1,17 @@
 from run import run
+import os
+import sys
 
 if __name__ == "__main__":
 
+    # Get the directory where the executable is located
+    exe_dir = os.path.dirname(sys.executable)
+
+    # Change the working directory to the directory where the executable is located
+    os.chdir(exe_dir)
+
     #input a file and create a run obj
-    this_run = run ("TESTRUN.RUN")
+    this_run = run (input("Enter Run file: "))
 
     #get the run output
     this_run.output()
